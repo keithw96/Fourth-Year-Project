222b89732c3a7bac5f0cd0c169140976356c0d0a
keithw96 <c00205321@itcarlow.ie>
2020-01-12 17:15:42 +0000

    Build works on Rpi

---
+++ FYP/CMakeFiles/FYP.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/projects/Fourth-Year-Project/FYP/Game.cpp" "/home/pi/projects/Fourth-Year-Project/FYP/CMakeFiles/FYP.dir/Game.cpp.o"
+  "/home/pi/projects/Fourth-Year-Project/FYP/Main.cpp" "/home/pi/projects/Fourth-Year-Project/FYP/CMakeFiles/FYP.dir/Main.cpp.o"
+  "/home/pi/projects/Fourth-Year-Project/FYP/Map.cpp" "/home/pi/projects/Fourth-Year-Project/FYP/CMakeFiles/FYP.dir/Map.cpp.o"
+  "/home/pi/projects/Fourth-Year-Project/FYP/Player.cpp" "/home/pi/projects/Fourth-Year-Project/FYP/CMakeFiles/FYP.dir/Player.cpp.o"
+  "/home/pi/projects/Fourth-Year-Project/FYP/TextureManager.cpp" "/home/pi/projects/Fourth-Year-Project/FYP/CMakeFiles/FYP.dir/TextureManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FYP/CMakeFiles/FYP.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FYP.dir/Main.cpp.o"
+  "CMakeFiles/FYP.dir/Game.cpp.o"
+  "CMakeFiles/FYP.dir/Player.cpp.o"
+  "CMakeFiles/FYP.dir/Map.cpp.o"
+  "CMakeFiles/FYP.dir/TextureManager.cpp.o"
+  "FYP.pdb"
+  "FYP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FYP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
