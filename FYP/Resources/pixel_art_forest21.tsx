9df41a17c3f708a4f11d0e0f56dabc78c3f1e182
Keith Wilson <c00205321@itcarlow.ie>
2020-01-28 09:26:50 +0000

    new tilesheet

--- FYP/Resources/pixel_art_forest21.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="pixel_art_forest21" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="../../../../../../Pictures/pixel_art_forest21.png" trans="ff00ff" width="96" height="94"/>
-</tileset>
